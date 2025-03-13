fb313bb0c50fe2e87eb2aca36cff3b648d118fb2
lenali26 <145240584+lenali26@users.noreply.github.com>
2025-03-13 14:11:00 -0700

    prints the positions of it

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,65 +1,66 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class p2 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//System.out.println("p2");
 		
-		readMap("src/Test01");
-		readMap("src/Test02");
-		readMap("src/Test03");
-		readMap("src/Test04");
-		readMap("src/Test05");
-		
+		readMap("src/TEST01");
+//		readMap("src/TEST02");
+//		readMap("src/TEST03");
+//		readMap("src/TEST04");
+		//readMap("src/TEST05");
 	}
 	
 	public static void readMap(String filename) {
 		
 		try {
-			File file = new File(filename); 
+			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
 			
-			
-			int numRows = scanner.nextInt(); 
-			int numCols = scanner.nextInt(); 
+			int numRows  = scanner.nextInt();
+			int numCols  = scanner.nextInt();
 			int numRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			int roomIndex = 0;
+			int colIndex = 0;
 			
 			Tile[][][] tiles = new Tile[numRows][numCols][numRooms];
 			
-			int rowIndex = 0;
-			int colsIndex = 0; 
-			int roomsIndex = 0; 
+			//process the map
 			
-			//process the map 
-			while(scanner.hasNextLine()) {
+			while (scanner.hasNextLine()) {
+				
 				//grab a line (one row of the map)
 				String row = scanner.nextLine();
 				System.out.println(row);
 				
-				if(row.length() > 0) 
-					for(int i = 0; i < numCols && i < row.length(); i++) {
-						char el = row.charAt(i); 
-						Tile obj = new Tile(rowIndex, i, el); 
+				if (row.length()>0) {
+					
+					for(colIndex = 0; colIndex < numCols && colIndex < row.length(); colIndex++) {
 						
+						char el = row.charAt(colIndex);
+						Tile obj = new Tile(rowIndex, colIndex, el);
+						tiles[rowIndex][colIndex][roomIndex] = new Tile(rowIndex, colIndex, el);
 						
-						if(obj.getType() == 'W') {
-							System.out.println("(" + rowIndex + "," + i + ")");
-						}
+						System.out.println("Tile at (" + tiles[rowIndex][colIndex][roomIndex].getRow()
+								+ ", " + tiles[rowIndex][colIndex][roomIndex].getCol() + ", "
+								+ tiles[rowIndex][colIndex][roomIndex].getType() + ")");
 						
 					}
+					
 					rowIndex++;
 				}
+			}
 			
-		} catch (FileNotFoundException e) {
-			//handle exception
+		}catch (FileNotFoundException e){
 			System.out.println(e);
 		}
-		
 		
 	}
 	
 	
 }
+
