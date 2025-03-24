8d3dee8c57db154dbc9741dd4d49ddbb15dd501b
lenali26 <145240584+lenali26@users.noreply.github.com>
2025-03-24 14:03:56 -0700

    Added the queue, that I refined from my old p2 class

--- P2_first_last/src/PathFinder.java
+++ P2_first_last/src/PathFinder.java
@@ -47,6 +47,48 @@ public class PathFinder {
 
         return false; //no path found or cannot find coins :(
     }
+    
+    public static boolean Queue(String[][] map, int startX, int startY, int endX, int endY) {
+        Queue<int[]> queue = new LinkedList<>(); //create a stack to store data/position/coordinate
+        boolean[][] visited = new boolean[map.length][map[0].length];//track visited
+
+        queue.add(new int[]{startX, startY}); //Start from the beginning
+        visited[startX][startY] = true; //marked that this position has already visited
+
+        while (!queue.isEmpty()) { //if stack isn't empty 
+            //poll the last position
+            //poll() = dequeue()
+            int[] current = queue.poll();
+            //current row and col
+            int r = current[0];
+            int c = current[1];
+
+            if (r == endX && c == endY) { //check have we reached the coin or not
+                return true; //YAY we reached the coin!!! 
+            }
+            
+            //Check all direction - North, South, East, West
+            for (int i = 0; i < 4; i++) {
+                //new row and new col
+                int newR = r + dRow[i]; 
+                int newC = c + dCol[i];
+                
+                //check is the new row and new col is able to move
+                if (isValidMove(map, newR, newC, visited)) {
+                    //add to stack
+                    queue.add(new int[]{newR, newC});
+                    //mark that it is already visited, so we will not revisit it
+                    visited[newR][newC] = true;
+                    //mark the path
+                    map[newR][newC] = "+"; 
+                }
+            }
+        }
+
+        return false; //no path found or cannot find coins :(
+    }
+    
+
     //check is it valid to move
     public static boolean isValidMove(String[][] map, int r, int c, boolean[][] visited) {
         if (r >= 0 && r < map.length && c >= 0 && c < map[0].length
