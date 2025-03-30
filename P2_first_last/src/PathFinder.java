879dfad144296c4114b97ed134d149daadc7c63a
lenali26 <145240584+lenali26@users.noreply.github.com>
2025-03-29 22:07:29 -0700

    added the queue, it's very similar to the other functions that I already made

--- P2_first_last/src/PathFinder.java
+++ P2_first_last/src/PathFinder.java
@@ -173,6 +173,89 @@ public class PathFinder {
         return true;	
     }
     
+    public static boolean Queue(String[][] map, int startX, int startY, int endX, int endY) {
+    	Queue<int[]> queue = new LinkedList<>(); //create a new linked list to store data/position/coordinates of the Wolverine
+    	boolean[][] visited = new boolean[map.length][map[0].length]; //setting the visited tracks 
+    	boolean found = false; //initializing the found path, the original initializing needs to be false! 
+    	
+    	int[][][] parentFunction = new int[map.length][map[0].length][2];
+    	//add the parent function in order to have multiple paths
+    	//this is used to retrace the shortest path! 
+    	for (int[][] row : parentFunction) {
+    		for (int[] row1: row) {
+    			Arrays.fill(row1, -1);
+    		}
+    	}
+    	//implementing push 
+        //push means that something is added to the beginning
+    	//push is the same as add! 
+        queue.add(new int[] {startX, startY}); 
+        visited[startX][startY] = true; 
+        //mark that the original starting point has already been visited
+        
+        while (!queue.isEmpty()) { //if stack is not empty 
+        	//isEmpty is the only function that can check if it is empty so you have to fix the object to be not to check that it isn't empty 
+        	
+        	//dequeue the last position
+        	//poll = dequeue 
+        	int[] current = queue.poll(); 
+        	
+        	//initialize the current values
+        	int row = current[0]; 
+        	int col = current[1]; 
+        	
+        	if (row == endX && col == endY) { 
+        		//check if the end values are reached in each the column and the row 
+        		found = true; //the coin is found, the function can stop
+        		break; 
+        		
+        	}
+        	for(int i = 0; i < 4; i++) { //uses 4 because you need to check all the directions: N/E/S/W
+        		
+        		//set a new row and new column if the four directions are being checked 
+        		int newRow = row + dRow[i]; 
+        		int newCol = col + dCol[i];
+        		
+        		//check if the new position is on the map! 
+        		if (isValidMove(map, newRow, newCol, visited)) {
+        			
+        			//uses the push command to add to the stack
+        			queue.add(new int[] {newRow, newCol});
+        			//mark that the position is already visited
+        			//mark position in order to make sure that it is not revisited
+        			visited[newRow][newCol] = true; 
+        			
+        			//changing the value of the parent function to continue iterating
+        			parentFunction[newRow][newCol][0] = row; 
+        			parentFunction[newRow][newCol][1] = row; 
+        			
+        		}
+        	}	
+        }
+        if (!found) {
+        	return false; // if a path is not found then false is returned! 
+        }
+        
+        List<int[]> path = new ArrayList<>(); 
+        //store the path
+        //uses a list because we want to store it in coordinate form! 
+        int row = endX, col = endY; 
+        //if it doesn't start from the beginning
+        while (!(row == startX && col == startY)) {
+        	//setting the previous function to the old function
+        	int prevRow = parentFunction[row][col][0]; 
+        	int prevCol = parentFunction[row][col][1]; 
+        	//change the row and column to the previous ones
+        	row = prevRow; 
+        	col = prevCol;
+        }
+        
+        for (int i = path.size() - 1; i >= 0; i--) {
+            int[] cell = path.get(i);
+            System.out.println("+ " + cell[0] + " " + cell[1] + " 0");
+        }
+        return true;
+    }
     
     public static boolean isValidMove(String[][] map, int row, int col, boolean[][] visited) {
     	//use recursion to check if the move is on the map! 
